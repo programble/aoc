76b6d23fdd58dbce87c35611651a101e7bd70b83
Curtis McEnroe <programble@gmail.com>
2016-12-05 21:47:56 -0500

    Day 5 part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -5,7 +5,7 @@ use std::io::{self, Read};
 use crypto::digest::Digest;
 use crypto::md5::Md5;
 
-fn solve(input: &str) -> String {
+fn solve1(input: &str) -> String {
     let mut password = String::new();
     let mut index = 0u64;
 
@@ -25,15 +25,47 @@ fn solve(input: &str) -> String {
     password
 }
 
+fn solve2(input: &str) -> String {
+    let mut password = [None; 8];
+    let mut index = 0u64;
+
+    while !password.iter().all(Option::is_some) {
+        let mut md5 = Md5::new();
+        md5.input_str(input);
+        md5.input_str(&index.to_string());
+        let digest = md5.result_str();
+
+        if &digest[0..5] == "00000" {
+            if let Some(pos @ 0 ... 7) = digest[5..6].parse().ok() {
+                password[pos] = password[pos].or(digest[6..7].chars().next());
+            }
+        }
+
+        index += 1;
+    }
+
+    password.iter()
+        .cloned()
+        .map(Option::unwrap)
+        .collect()
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
-    println!("Part 1: {}", solve(input.trim()));
+    println!("Part 1: {}", solve1(input.trim()));
+    println!("Part 2: {}", solve2(input.trim()));
 }
 
 #[test]
 #[ignore]
 fn part1() {
-    assert_eq!("18f47a30", solve("abc"));
+    assert_eq!("18f47a30", solve1("abc"));
+}
+
+#[test]
+#[ignore]
+fn part2() {
+    assert_eq!("05ace8e3", solve2("abc"));
 }
