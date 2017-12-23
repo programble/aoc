fe31ed565b9bc5c4a5b74d6898f4f93c523c5b13
Curtis McEnroe <programble@gmail.com>
2017-12-23 17:33:41 -0500

    Day 13, part 2

--- 2017/src/bin/day13.rs
+++ 2017/src/bin/day13.rs
@@ -48,16 +48,64 @@ fn solve1(input: &str) -> usize {
     severity
 }
 
+fn solve2(input: &str) -> usize {
+    let mut init_layers = vec![];
+    for line in input.lines() {
+        let mut iter = line.split(": ");
+        let index: usize = iter.next().unwrap().parse().unwrap();
+        let range = iter.next().unwrap().parse().unwrap();
+        init_layers.resize(index + 1, None);
+        init_layers[index] = Some(Layer::new(range));
+    }
+
+    'delay: for delay in 0.. {
+        if delay > 0 {
+            for layer in &mut init_layers {
+                layer.as_mut().map(Layer::step);
+            }
+        }
+
+        let mut layers = init_layers.clone();
+        for i in 0..layers.len() {
+            if let Some(ref layer) = layers[i] {
+                if layer.scanner == 0 {
+                    continue 'delay;
+                }
+            }
+
+            for layer in &mut layers {
+                layer.as_mut().map(Layer::step);
+            }
+        }
+
+        return delay;
+    }
+    unreachable!()
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
     println!("Part 1: {}", solve1(&input));
+    println!("Part 2: {}", solve2(&input));
 }
 
 #[test]
 fn part1() {
     assert_eq!(24, solve1(
+"\
+0: 3
+1: 2
+4: 4
+6: 4
+"
+    ));
+}
+
+#[test]
+fn part2() {
+    assert_eq!(10, solve2(
 "\
 0: 3
 1: 2
