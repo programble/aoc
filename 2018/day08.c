5f306248a8aa5adac034e137515ed740d9744fbe
Curtis McEnroe <june@causal.agency>
2018-12-08 21:51:37 -0500

    Solve day 8 part 2

--- 2018/day08.c
+++ 2018/day08.c
@@ -5,24 +5,40 @@ typedef unsigned uint;
 
 static uint sum;
 
-static void meta(void) {
+static uint meta(void) {
 	uint meta;
 	scanf("%u", &meta);
 	sum += meta;
+	return meta;
 }
 
-static void node(void) {
+static uint node(void) {
 	uint nchild, nmeta;
 	scanf("%u %u", &nchild, &nmeta);
+	uint children[nchild];
+	uint metas[nmeta];
 	for (uint i = 0; i < nchild; ++i) {
-		node();
+		children[i] = node();
 	}
 	for (uint i = 0; i < nmeta; ++i) {
-		meta();
+		metas[i] = meta();
 	}
+	uint val = 0;
+	if (nchild) {
+		for (uint i = 0; i < nmeta; ++i) {
+			if (!metas[i] || metas[i] > nchild) continue;
+			val += children[metas[i] - 1];
+		}
+	} else {
+		for (uint i = 0; i < nmeta; ++i) {
+			val += metas[i];
+		}
+	}
+	return val;
 }
 
 int main() {
-	node();
+	uint val = node();
 	printf("%u\n", sum);
+	printf("%u\n", val);
 }
